91f804d8d91d30e015ecfd860c667e79f03a15ef
Sulkhani <s.otkhozoria.exceedteam@gmail.com>
2020-03-27 13:20:16 +0400

    add edit and check buttons, add styles

--- app.js
+++ app.js
@@ -3,22 +3,60 @@ const addBtn = document.querySelector('#add');
 const list = document.querySelector('#list');
 
 addBtn.addEventListener('click', add);
+field.addEventListener('keyup', add);
 
-function add() {
-    if (field.value) {
-        let item = document.createElement('li');
-        item.innerHTML = field.value;
-        list.append(item);
-        let rmvBtn = document.createElement('button')
-        rmvBtn.classList.add('remove');
-        rmvBtn.innerHTML = 'remove';
-        rmvBtn.addEventListener('click', () => {
-            item.parentNode.removeChild(item);
-        })
-        item.append(rmvBtn);
-        field.value = '';
-    } else {
-        return
+function add(e) {
+    if (e.keyCode == 13 || e.type == 'click') {
+        if (field.value.trim() != 0) {
+            let item = document.createElement('li');
+            list.append(item);
+            let span = document.createElement('span');
+            span.innerHTML = field.value;
+            item.append(span)
+            let checkBtn = document.createElement('input');
+            checkBtn.setAttribute('type', 'checkbox');
+            checkBtn.addEventListener('change', () => {
+                if (checkBtn.checked) {
+                    span.classList.add('done')
+                } else {
+                    span.classList.remove('done');
+                }
+            })
+            item.append(checkBtn);
+            let editBtn = document.createElement('button');
+            editBtn.innerHTML = 'edit';
+            let editInput = document.createElement('input');
+            editInput.setAttribute('type', 'text');
+            item.append(editInput)
+            editInput.style.display = 'none';
+            editBtn.addEventListener('click', () => {
+                span.style.display = 'none';
+                checkBtn.style.display = "none"
+                editInput.style.display = 'inline-block';
+                editInput.addEventListener('keyup', (e) => {
+                    if (e.keyCode == 13) {
+                        span.innerHTML = editInput.value;
+                        span.style.display = '';
+                        editInput.style.display = 'none';
+                        checkBtn.style.display = ""
+                    }
+                })
+
+
+            })
+            item.append(editBtn);
+            let rmvBtn = document.createElement('button')
+            rmvBtn.classList.add('remove');
+            rmvBtn.innerHTML = 'remove';
+            rmvBtn.addEventListener('click', () => {
+                item.parentNode.removeChild(item);
+            })
+            item.append(rmvBtn);
+
+            field.value = '';
+        }
     }
+
+
 
 }
