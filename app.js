09da10b3df35df99b422fb3554967b88a56a2f0b
Sulkhani <s.otkhozoria.exceedteam@gmail.com>
2020-03-27 10:51:40 +0400

    initial code

---
+++ app.js
@@ -0,0 +1,23 @@
+const field = document.querySelector('#field');
+const addBtn= document.querySelector('#add');
+const list = document.querySelector('#list');
+
+
+function add(e){
+    let item = document.createElement('li');
+    item.innerHTML = `${field.value} <button class="remove">Remove</button>`;
+    list.append(item);
+    let removeBtns = document.querySelectorAll('.remove');
+    removeBtns.forEach((e,idx)=>{
+        e.addEventListener('click',function(){
+            console.log(e.parentElement)
+            list.children[idx].remove()
+        })
+    })
+        
+    }
+    
+  
+
+  
+  addBtn.addEventListener('click',add);
